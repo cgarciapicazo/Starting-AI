2dde90058c39d92c3defd3c3bc83fd1308a137b5
Carlos Garcia <carlos@Macbook-Pro.local>
2026-01-28 21:20:57 +0000

    Training in batches to prevent overfitting

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 IMG_SIZE = (64, 64)
-NUM_EPOCHS = 1000
-LEARNING_RATE = 1e-3
-EPOCH_CHECK_INTERVAL = 20
 AGE_CLASSES = ["18-20", "21-30", "31-40", "41-50", "51-60"]
 AGE_TO_INDEX = {label: i for i, label in enumerate(AGE_CLASSES)}
+
+NUM_EPOCHS = 1000
+LEARNING_RATE = 1e-3
+BATCH_SIZE = 10
+EPOCH_CHECK_INTERVAL = 50
+SHOW_GRAPH = True
+
 
 def load_data():
     df = pd.read_csv("archive/age_detection.csv")
@@ -22,7 +26,6 @@ def load_data():
             img = ImageOps.exif_transpose(img)
             img = img.convert("RGB")
             img = img.resize(IMG_SIZE)
-            # Model-friendly format: float32 in [0, 1], flattened (H*W*C)
             vec = (np.asarray(img, dtype=np.float32) / 255.0).flatten()
             X.append(vec)
             
@@ -35,6 +38,35 @@ def display_picture(vec, label = ""):
     plt.imshow(vec.reshape(IMG_SIZE[0], IMG_SIZE[1], 3))
     plt.title(label)
     plt.show()
+
+def train_model():
+    loss_graph = []
+    for epoch in range(NUM_EPOCHS):
+        optimiser.zero_grad()
+
+        idx = torch.randint(0, X_train_tensor.size(0), (BATCH_SIZE,))
+        xb = X_train_tensor[idx]
+        yb = y_train_tensor[idx]
+
+        predictions = Model(xb)
+        crossent = loss(predictions, yb)
+
+        crossent.backward()
+        optimiser.step()
+
+        if epoch % EPOCH_CHECK_INTERVAL == 0:
+            print(f"epoch: {epoch}, loss: {crossent}")
+            loss_graph.append(float(crossent.item()))
+    if SHOW_GRAPH:
+        plt.plot(np.linspace(0, NUM_EPOCHS, int(NUM_EPOCHS / EPOCH_CHECK_INTERVAL)), loss_graph)
+        plt.show()
+    
+def evaluate_model(X, y):
+    Model.eval()
+    with torch.no_grad():
+        predictions = Model(X)
+        testloss = loss(predictions, y)
+        return testloss
 
 class NN_Regression(torch.nn.Module):
 
@@ -65,18 +97,5 @@ if __name__ == "__main__":
     Model = NN_Regression()
     loss = torch.nn.CrossEntropyLoss()
     optimiser = torch.optim.Adam(Model.parameters(), lr=LEARNING_RATE)
+    train_model()
 
-    loss_graph = []
-    for epoch in range(NUM_EPOCHS):
-        optimiser.zero_grad()
-
-        predictions = Model(X_train_tensor)
-        crossent = loss(predictions, y_train_tensor)
-
-        crossent.backward()
-        optimiser.step()
-        if epoch % EPOCH_CHECK_INTERVAL == 0:
-            print(f"epoch: {epoch}, loss: {crossent}")
-            loss_graph.append(float(crossent.values()))
-    plt.plot(np.linspace(EPOCH_CHECK_INTERVAL, NUM_EPOCHS, EPOCH_CHECK_INTERVAL), loss_graph)
-    plt.show()
