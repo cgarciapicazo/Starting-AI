2a8919c833b648e90dee544093595caafa5c1ea9
Carlos Garcia <carlos@Macbook-Pro.local>
2026-01-28 16:26:15 +0000

    Randomised Data Set

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ img_size = (64, 64)
 
 def load_data():
     df = pd.read_csv("archive/age_detection.csv")
+    train = df[df["split"] == "train"].sample(frac=1)
 
     X = []
-    X_file = df[df["split"] == "train"]["file"]
+    X_file = train["file"]
     for img_path in X_file:
         with Image.open("archive/" + img_path) as img:
             img = ImageOps.exif_transpose(img)
@@ -18,15 +19,16 @@ def load_data():
             vec = np.asarray(img, dtype=np.uint8).flatten()
             X.append(vec)
             
-    y = df[df["split"] == "train"]["age"]
+    y = train["age"].to_numpy()
 
     return X, y
 
-def display_picture(vec):
+def display_picture(vec, label = ""):
     plt.imshow(vec.reshape(img_size[0], img_size[1], 3))
+    plt.title(label)
     plt.show()
 
 if __name__ == "__main__":
     X_train, y_train = load_data()
-    for f in X_train[:5]:
-        display_picture(f)
+    for i in range(50, 53):
+        display_picture(X_train[i], label=y_train[i])
