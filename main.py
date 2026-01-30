558015f6901ccfb6b1a87c16c62ecdbb2f2cfc4b
Carlos Garcia <carlos@Macbook-Pro.local>
2026-01-30 18:22:20 +0000

    Simple AI functionality

--- main.py
+++ main.py
@@ -8,19 +8,23 @@ IMG_SIZE = (64, 64)
 AGE_CLASSES = ["18-20", "21-30", "31-40", "41-50", "51-60"]
 AGE_TO_INDEX = {label: i for i, label in enumerate(AGE_CLASSES)}
 
-NUM_EPOCHS = 1000
+NUM_EPOCHS = 100
 LEARNING_RATE = 1e-3
-BATCH_SIZE = 10
+BATCH_SIZE = 5
 EPOCH_CHECK_INTERVAL = 50
 SHOW_GRAPH = True
 
 
-def load_data():
+def load_data(test_data):
     df = pd.read_csv("archive/age_detection.csv")
-    train = df[df["split"] == "train"].sample(frac=1)
+    if test_data:
+        select = "test"
+    else:
+        select = "train"
+    select_df = df[df["split"] == select]
 
     X = []
-    X_file = train["file"]
+    X_file = select_df["file"]
     for img_path in X_file:
         with Image.open("archive/" + img_path) as img:
             img = ImageOps.exif_transpose(img)
@@ -29,10 +33,16 @@ def load_data():
             vec = (np.asarray(img, dtype=np.float32) / 255.0).flatten()
             X.append(vec)
             
-    y_str = train["age"].to_numpy()
+    y_str = select_df["age"].to_numpy()
     y = np.array([AGE_TO_INDEX[label] for label in y_str], dtype=np.int64)
 
     return X, y
+
+def to_tensor(X, y):
+        X_tensor = torch.tensor(np.array(X), dtype=torch.float32)
+        y_tensor = torch.tensor(y, dtype=torch.long)
+        return X_tensor, y_tensor
+
 
 def display_picture(vec, label = ""):
     plt.imshow(vec.reshape(IMG_SIZE[0], IMG_SIZE[1], 3))
@@ -66,7 +76,7 @@ def evaluate_model(X, y):
     with torch.no_grad():
         predictions = Model(X)
         testloss = loss(predictions, y)
-        return testloss
+        return testloss.item()
 
 class NN_Regression(torch.nn.Module):
 
@@ -88,14 +98,18 @@ class NN_Regression(torch.nn.Module):
         return x
 
 if __name__ == "__main__":
-    X_train, y_train = load_data()
+    X_train, y_train = load_data(test_data=False)
+    X_test, y_test = load_data(test_data=True)
+
     # for i in range(50, 53):
     #     display_picture(X_train[i], label=y_train[i])
-    X_train_tensor = torch.tensor(np.array(X_train), dtype=torch.float32)
-    y_train_tensor = torch.tensor(y_train, dtype=torch.long)
+
+    X_train_tensor , y_train_tensor= to_tensor(X_train, y_train)
+    X_test_tensor, y_test_tensor = to_tensor(X_test, y_test)
 
     Model = NN_Regression()
     loss = torch.nn.CrossEntropyLoss()
     optimiser = torch.optim.Adam(Model.parameters(), lr=LEARNING_RATE)
     train_model()
+    print(evaluate_model(X_train_tensor, y_train_tensor), evaluate_model(X_test_tensor, y_test_tensor))
 
